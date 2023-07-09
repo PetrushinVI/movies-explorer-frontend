4e3e2e79bcf147b58ab7a2769ba6fd0e3fcafebc
Владислав <alina220578@mail.ru>
2023-07-09 03:55:47 +0300

    verstka/iter2

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import './MoviesCard.css';
 import cardImage from '../../images/card.jpg';
 
 
 function MoviesCard() {
 
+  const location = useLocation();
+  const [liked, setLiked] = useState(false);
+
   return(
     <li className='card'>
       <img className='card__image' src={cardImage} alt='Изображение карточки'/>
       <div className='card__description'>
         <h2 className='card__name'>33 слова о дизайне</h2>
-        <button className='card__like card__like_type_delete'>
-          <></>
-        </button>
+        <button
+          className={`card__like ${
+            location.pathname === '/movies'
+              ? `card__like ${liked ? 'card__like_type_active' : ''}`
+              : 'card__like_type_delete'
+          }`}
+          type='button'
+          onClick={() => (!liked ? setLiked(true) : setLiked(false))}
+        />
       </div>
       <span className='card__duration'>1ч42м</span>
     </li>
